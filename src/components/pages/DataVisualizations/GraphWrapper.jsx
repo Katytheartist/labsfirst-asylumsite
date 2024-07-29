3421863e5a74332af570868be9835129a2734b87
Katy Kane <katytheartist@gmail.com>
2024-07-28 20:53:13 -0500

    almost done

--- src/components/pages/DataVisualizations/GraphWrapper.jsx
+++ src/components/pages/DataVisualizations/GraphWrapper.jsx
@@ -10,11 +10,13 @@ import YearLimitsSelect from './YearLimitsSelect';
 import ViewSelect from './ViewSelect';
 import axios from 'axios';
 import { resetVisualizationQuery } from '../../../state/actionCreators';
-import test_data from '../../../data/test_data.json';
+//import test_data from '../../../data/test_data.json';
 import { colors } from '../../../styles/data_vis_colors';
 import ScrollToTopOnMount from '../../../utils/scrollToTopOnMount';
 
 const { background_color } = colors;
+  //main url 
+  const mainUrl = 'https://hrf-asylum-be-b.herokuapp.com/cases'; 
 
 function GraphWrapper(props) {
   const { set_view, dispatch } = props;
@@ -50,7 +52,13 @@ function GraphWrapper(props) {
         break;
     }
   }
-  function updateStateWithNewData(years, view, office, stateSettingCallback) {
+
+  //fetch data from api and update state
+  async function updateStateWithNewData(
+    years, 
+    view, 
+    office, 
+    stateSettingCallback) {
     /*
           _                                                                             _
         |                                                                                 |
@@ -71,40 +79,96 @@ function GraphWrapper(props) {
           _                                                                             _
                                    -- Mack 
     
-    */
+    */                       
 
+
+  try{
+    //create holders for our new data we will grab
+    let fiscalData = {};
+    let citizenshipData = {};
+
+    console.log('fiscal data:', fiscalData);
+    console.log('citizenship data:', citizenshipData);
+
+    //fetch all fiscal data if no office selected
     if (office === 'all' || !office) {
-      axios
-        .get(process.env.REACT_APP_API_URI, {
-          // mock URL, can be simply replaced by `${Real_Production_URL}/summary` in prod!
-          params: {
-            from: years[0],
-            to: years[1],
-          },
-        })
-        .then(result => {
-          stateSettingCallback(view, office, test_data); // <-- `test_data` here can be simply replaced by `result.data` in prod!
-        })
-        .catch(err => {
-          console.error(err);
-        });
-    } else {
-      axios
-        .get(process.env.REACT_APP_API_URI, {
-          // mock URL, can be simply replaced by `${Real_Production_URL}/summary` in prod!
-          params: {
-            from: years[0],
-            to: years[1],
-            office: office,
-          },
-        })
-        .then(result => {
-          stateSettingCallback(view, office, test_data); // <-- `test_data` here can be simply replaced by `result.data` in prod!
-        })
-        .catch(err => {
-          console.error(err);
-        });
+      const fiscalRes = await axios.get(`${mainUrl}/fiscalSummary`, {
+        params: { //set query params from 1st el in yrs arr to 2nd el
+          from: years[0],
+          to: years[1],
+        },
+      });
+
+      fiscalData = fiscalRes.data; //setting fis data to have the actual data we have fetched for it
+      console.log('fiscal data for all offices:', fiscalData);
+    } else { 
+      //then fetch fis data for specific office
+      const fiscalRes = await axios.get (`${mainUrl}/fiscalSummary`, {
+        params: {
+          from: years[0],
+          to: years[1],
+          office,
+        },
+      });
+
+      fiscalData = fiscalRes.data;
+      console.log(`fiscal data for ${office} :`, fiscalData);
+
+      //fetch cit data for specific off
+      const citizenshipRes = await axios.get(`${mainUrl}/citizenshipSummary`, {
+        params: {
+          from: years[0],
+          to: years[1],
+          office,
+        },
+      });
+      //filling our previously created var with the cit data we just fetched
+      citizenshipData = citizenshipRes.data;
+      console.log(`citizenship data from ${office}:`, citizenshipData);
     }
+
+    //combine data into single obj
+    const allData = { ...fiscalData, citizenshipResults: citizenshipData};
+    console.log('new data obj:', allData);
+
+    //invoke the callback to set state with fetched data
+    stateSettingCallback(view, office, [allData]);
+  } catch (err) {
+    console.error('error fetching data:', err);
+  }                              
+   // check if office is set to all or null
+    // if (office === 'all' || !office) {
+    //    axios.get(
+    //     `${mainUrl}/fiscalSummary`, {
+          
+    //       params: {
+    //         from: years[0],
+    //         to: years[1],
+    //       },
+    //     })
+    //     .then(result => {
+    //       stateSettingCallback(view, office, [result.data]); // <-- `test_data` here can be simply replaced by `result.data` in prod!
+    //     })
+    //     .catch(err => {
+    //       console.error(err);
+    //     });
+    // } else {
+    //   axios
+    //     .get(`${mainUrl}/citizenshipSummary`, {
+    //       // mock URL, can be simply replaced by `${Real_Production_URL}/summary` in prod!
+    //       params: {
+    //         from: years[0],
+    //         to: years[1],
+    //         office: office,
+    //       },
+    //     })
+    //     .then(result => {
+    //       stateSettingCallback(view, office, [result.data]); // <-- `test_data` here can be simply replaced by `result.data` in prod!
+    //     })
+    //     .catch(err => {
+    //       console.error(err);
+    //     });
+    // }
   }
   const clearQuery = (view, office) => {
     dispatch(resetVisualizationQuery(view, office));
